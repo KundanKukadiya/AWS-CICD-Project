6ba5990f5b92fc39fff2ffabe21e86849fd21d16
Kundan Kukadiya <41021278+KundanKukadiya@users.noreply.github.com>
2024-01-11 16:38:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return 'Hello World! I have been seen you in just {} times.\n'.format(count)
+    return 'Hello World! I am Kundan and you have been seen you in just {} times.\n'.format(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
