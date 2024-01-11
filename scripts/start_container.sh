bba1bf3d1ca13bf0be38c8563c6c351c87bf1af0
Kundan Kukadiya <41021278+KundanKukadiya@users.noreply.github.com>
2024-01-11 14:07:49 +0530

    files added for code deploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# pull the Docker image from Docker hub
+docker push kundan2312/sample-python-app
+
+# Run the Docker image as container
+docker run -d -p 8000:8000 kundan2312/sample-python-app
---
+++ scripts/stop_container.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+# stop the running container
+echo "Hi"
