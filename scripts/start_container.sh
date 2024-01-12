7cf0fe68d9afedb994c93899d31ecc2b7cc0733c
Kundan Kukadiya <41021278+KundanKukadiya@users.noreply.github.com>
2024-01-12 10:09:23 +0530

    some changes done

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # pull the Docker image from Docker hub
-docker push kundan2312/sample-python-app
+docker pull kundan2312/sample-python-app
 
 # Run the Docker image as container
 docker run -d -p 8000:8000 kundan2312/sample-python-app
